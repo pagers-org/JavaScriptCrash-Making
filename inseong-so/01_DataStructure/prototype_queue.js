a1b1442e99e45463ee125fee7c1298999e707fb1
InSeong-So <goflvhxj2547@gmail.com>
2021-10-11 13:26:44 +0900

    ⌨️ 자료구조 구현(스택, 큐)

---
+++ inseong-so/01_DataStructure/prototype_queue.js
@@ -0,0 +1,34 @@
+function Queue() {
+  this.array = [];
+  this._size = 0;
+}
+
+Queue.prototype.append = function (item) {
+  this.array.unshift(item);
+  this._size++;
+};
+
+Queue.prototype.popleft = function () {
+  this._size--;
+  return this.array.shift();
+};
+
+Queue.prototype.size = function () {
+  return this._size;
+};
+
+Queue.prototype.peek = function () {
+  return this.array[this._size - 1];
+};
+
+const queue = new Queue();
+queue.append(1);
+queue.append(2);
+console.log(queue.shift());
+queue.append(3);
+queue.append(4);
+console.log(queue.shift());
+
+while (queue.size()) {
+  console.log(queue.shift());
+}
