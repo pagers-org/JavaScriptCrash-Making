f262502153fd75dcdf9d3ccb9b930010b3a222e8
Heejin Na <heejin.info@gmail.com>
2021-10-11 23:27:58 +0900

    feat: sum

--- sori/02_Functions/01.js
+++ sori/02_Functions/01.js
@@ -6,15 +6,20 @@ const reverse = array => array.sort((a, b) => b - a);
 console.log(reverse(arr));
 
 // 평균 구하기 : 5.5
-const average = array => array.reduce((p, c) => p + c, 0) / array.length;
+const average = array =>
+  array.reduce((acc, cur) => acc + cur, 0) / array.length;
 console.log(average(arr));
 
-// 최대값-최소값 구하기 : 1, 10
-const min = Math.min.apply(0, arr);
+// 최대값-최소값 구하기 : 9
 const max = Math.max.apply(0, arr);
-console.log(min, max);
+const min = Math.min.apply(0, arr);
+console.log(max - min);
 
-// 누적합
+// 누적합 : 55
+const sum = array => array.reduce((acc, cur) => acc + cur, 0);
+console.log(sum(arr));
+
+// 배열 정렬하기
 
 // 중복 횟수
 
